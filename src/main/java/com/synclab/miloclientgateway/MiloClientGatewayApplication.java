adb8a9ce372396114345167bd6010474994e07b8
dobbyrr <zxr1033@gmail.com>
2025-11-10 16:18:43 +0900

    [Feat] 스케줄링 기능 활성화를 위한 @EnableScheduling 추가

--- src/main/java/com/synclab/miloclientgateway/MiloClientGatewayApplication.java
+++ src/main/java/com/synclab/miloclientgateway/MiloClientGatewayApplication.java
@@ -2,8 +2,10 @@ package com.synclab.miloclientgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MiloClientGatewayApplication {
 
     public static void main(String[] args) {
